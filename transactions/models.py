072a19c00fb5587c7b8dbd0ab763361d873e3445
Iván González <ivan.gonzalez@ug.uchile.cl>
2024-01-12 20:08:23 -0300

    chore: Fix import order

--- transactions/models.py
+++ transactions/models.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-
 import uuid
 
+from django.contrib.auth import get_user_model
+from django.db import models
 from django.utils import timezone
 
 
